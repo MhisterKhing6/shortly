d6dedb1e466cd993e64c3509909f78814f303811
Kingsley <dondecency11@gmail.com>
2025-12-22 09:57:39 +0000

    feat:cancelation reason dto validation

--- src/main/java/shortly/mandmcorp/dev/shortly/dto/request/CancelationReasonRequest.java
+++ src/main/java/shortly/mandmcorp/dev/shortly/dto/request/CancelationReasonRequest.java
@@ -1,8 +1,10 @@
 package shortly.mandmcorp.dev.shortly.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CancelationReasonRequest {
+    @NotBlank(message="Reason is required")
     private  String reason;
 }
